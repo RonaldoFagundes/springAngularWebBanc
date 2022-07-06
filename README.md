f6bf091d484fa759b6d21c0a7f5b8d139804adfe
RonaldoFagundes <ronaldofagundes@gmail.com>
2022-07-06 10:46:12 -0300

    update all

---
+++ README.md
@@ -0,0 +1,14 @@
+# Aplicação Full Stack  - em desenvolvimento -
+## simulador de um banco digital 
+
+&nbsp;
+ - cadastro
+ - login
+ - abertura de contas 
+ - movimentação
+
+  &nbsp;
+### tecnologias : 
+| front-end  : HTML5 , CSS3 , Bootstrap , ES6, Angular |   
+| back-end   :       java - Spring Boot                |      
+| banco de dados :     MySQL Workbench                 |
--- src/main/java/com/rfagundes/financial/domain/ContaBancaria.java
+++ src/main/java/com/rfagundes/financial/domain/ContaBancaria.java
@@ -1,15 +1,31 @@
 package com.rfagundes.financial.domain;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Objects;
 
-public class ContaBancaria {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
+@Entity
+public class ContaBancaria implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String tipo;
 	private String numero;
 	private BigDecimal saldo;
 
+	@ManyToOne
+	@JoinColumn(name="categoria_id")
 	private Categoria categoria;
 
 	public ContaBancaria() {
